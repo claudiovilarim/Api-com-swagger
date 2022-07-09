67713ccd57c29ce02ff6cd4ba6f1e0995d2a9f93
claudio vilarim <claudio.v.lima2@gmail.com>
2022-07-09 18:04:46 -0300

    first commit

---
+++ src/main/java/com/siga/apirest/config/SwaggerConfig.java
@@ -0,0 +1,47 @@
+package com.siga.apirest.config;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+import java.util.ArrayList;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+	
+	@Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.siga.apirest"))
+                .paths(regex("/api.*"))
+                .build()
+                .apiInfo(metaInfo());
+    }
+
+    private ApiInfo metaInfo() {
+
+        ApiInfo apiInfo = new ApiInfo(
+                "Produtos API REST",
+                "API REST de cadastro de produtos.",
+                "1.0",
+                "Terms of Service",
+                new Contact("Claudio Lima","https://github.com/claudiovilarim", "claudio.v.lima2@gmail.com"),
+                "Apache License Version 2.0",
+                "https://www.apache.org/", new ArrayList<VendorExtension>()
+        );
+
+        return apiInfo;
+    }
+
+}
---
+++ src/main/java/com/siga/apirest/repository/ProdutoRepository.java
@@ -0,0 +1,10 @@
+package com.siga.apirest.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.siga.apirest.models.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+	Produto findById(long id);
+}
